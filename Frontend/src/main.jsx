33a172f82163d11073083ef11b4a3966ced02cf8
Srijan <srijanspl2017@gmail.com>
2025-11-02 15:31:29 +0530

    Update main.jsx
    Updated server url on main.jsx since project is being uploaded on render.

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 
-export const serverURL = "http://localhost:3000";
+export const serverURL = "https://textin.onrender.com";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
