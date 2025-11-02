19700ae29a2dda655ce88599af072161bcadde81
Srijan <srijanspl2017@gmail.com>
2025-11-02 15:45:57 +0530

    Update createToken.js
    Changes according to the production environment

--- Backend/Config/createToken.js
+++ Backend/Config/createToken.js
@@ -6,7 +6,7 @@ export const createToken = (userId, res) => {
   res.cookie('jwt', token, {
     maxAge: 7*24*60*60*1000, // cokkie age in milli sec
     httpOnly:true, // protects against the XSS attacks
-    sameSite:"strict", // protects agains csrf
-    secure: process.env.NODE_ENV !== 'development'
+    sameSite:"None", // protects agains csrf
+    secure: true
   })
 }
