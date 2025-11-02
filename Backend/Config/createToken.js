b325d6434c74ecc3f6bc5ce69b1daf32d99bd1db
Srijan <srijanspl2017@gmail.com>
2025-11-02 16:11:27 +0530

    Update createToken.js

--- Backend/Config/createToken.js
+++ Backend/Config/createToken.js
@@ -7,6 +7,6 @@ export const createToken = (userId, res) => {
     maxAge: 7*24*60*60*1000, // cokkie age in milli sec
     httpOnly:true, // protects against the XSS attacks
     sameSite:"strict", // protects agains csrf
-    secure: true
+    secure: process.env.NODE_ENV !== 'development'
   })
 }
