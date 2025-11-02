e6341b1b077fe3829fe1b52429dca7496170a93c
Srijan <srijanspl2017@gmail.com>
2025-11-02 16:10:00 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 
-export const serverURL = "https://textin.onrender.com";
+export const serverURL = "http://localhost:3000";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
