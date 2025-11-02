284893fe4d8bd01fcbec02a5e23708915b0a0a16
Srijan <srijanspl2017@gmail.com>
2025-11-02 15:42:05 +0530

    Update server.js
    cors rule update

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ dotenv.config({ path: ".env" });
 
 // middlewares
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://textin-1.onrender.com",
   credentials:true
 }))
 app.use(bodyParser.json());
